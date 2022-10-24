0a966dfea9cede6a20998d98f38d63406da9c194
OSdoge <60252279+OSdoge@users.noreply.github.com>
2022-10-24 21:13:22 +0800

    Add files via upload

---
+++ OSdoge portfolio main public/index.js
@@ -0,0 +1,20 @@
+let sky = document.getElementById('sky');
+let clouds1 = document.getElementById('clouds1');
+let rocks = document.getElementById('rocks');
+let clouds2 = document.getElementById('clouds2');
+let ground1 = document.getElementById('ground1');
+let ground2 = document.getElementById('ground2');
+let ground3 = document.getElementById('ground3');
+let plant = document.getElementById('plant');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    clouds1.style.top = -value * 0.1 + 'px';
+    clouds2.style.top = -value * 0.1 + 'px';
+    ground1.style.top = -value * 0.2 + 'px';
+    ground2.style.top = -value * 0.3 + 'px';
+    ground3.style.top = -value * 0.3 + 'px';
+    plant.style.top = -value * 0.3 + 'px';
+})
+
+console.log("hi");
